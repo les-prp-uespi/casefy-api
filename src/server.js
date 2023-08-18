90339f96f68c6ae286e6d6ff4d18b9fb9c0bd37d
ohlavrac <joaovicrcarvalho@gmail.com>
2023-08-18 10:36:38 -0300

    update

--- src/server.js
+++ src/server.js
@@ -2,6 +2,14 @@ const express = require("express");
 const port = 3000;
 const server = express();
 
+const createClinicalCaseRoute = require("./routes/create_clinical_case_route.js");
+const getClinicalCaseRoute = require("./routes/get_clinical_case_route.js");
+
+server.use(express.json());
+
+server.use(createClinicalCaseRoute);
+server.use(getClinicalCaseRoute);
+
 server.get("/", (req, res) => {
     res.send("CASEFY API");
 });
