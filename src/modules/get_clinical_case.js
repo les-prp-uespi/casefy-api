fd68b55d9de64feb210febf9e8d90a5c4515f322
ohlavrac <joaovicrcarvalho@gmail.com>
2023-08-24 09:17:17 -0300

    add: get all clinical case data

--- src/modules/get_clinical_case.js
+++ src/modules/get_clinical_case.js
@@ -10,8 +10,39 @@ const getClinicalCaseById = async (
             }
         },
         include: {
-            pacient_data: true
-        }
+            pacient_data: true,
+            nutritional_clinical_anamnesis: {
+                include: {
+                    physical_activities: true
+                }
+            },
+            habits: true,
+            food_conditions: true,
+            anthropometric_assessment: {
+                include: {
+                    circumferences: true,
+                    skin_folds: true,
+                    anthropometric_evaluation_results: true,
+                }
+            },
+            laboratory_tests: true,
+            food_recall: {
+                include: {
+                    meals: true,
+                    total_gram_meal: true,
+                    geral: true,
+                    macronutrients: true,
+                    nutrients: true
+                }
+            },
+            food_plan: {
+                include: {
+                    geral: true,
+                    macronutrients: true,
+                    nutrients: true
+                }
+            },
+        },
     });
 
     if (findClinicalCase == false) {
